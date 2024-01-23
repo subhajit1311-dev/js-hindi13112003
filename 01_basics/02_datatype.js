4719e6f24730fd2ac750f80f68b31be61546b5d2
subhajit_dev <146475311+subhajit1311-dev@users.noreply.github.com>
2024-01-23 08:03:09 +0000

    datatype done

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all js code as newer version
+
+//  alert(3+3)  // we are  using nodejs not browser
+console.log(3 + 
+    3)
+
+// code readability should be high
+
+console.log("subhajit")
+let name = "subhajit"
+let age = 18
+let isloggedIn = false
+
+
+// PRIMITIVE DATATYPES
+//number = 2^53  
+//bigInt - big number er store  er jonno
+//string -> ""
+//boolean -> true/false
+//null -> standalone value , value assign here but for any unknown reason the value should not be 
+//assigned on the mean time
+//undefined -> variable declare here but value in the variable should not be assigned in that mean time 
+//symbol -> uniqueness
+
+
+
+
+
+//OBJECT 
+
+console.log(typeof "hitesh")
+
+console.log(typeof null) //important typeof null is a object
+
