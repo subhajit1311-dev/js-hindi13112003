34f8fa255ba589402d92633f4df92ec6808d8413
subhajit_dev <146475311+subhajit1311-dev@users.noreply.github.com>
2024-01-25 15:44:34 +0000

    summary of datatype done

---
+++ 01_basics/05_datatypesSummary.js
@@ -0,0 +1,48 @@
+// to define any datatypes in terms of store and access that data into the memory is of 
+//two types 1>primitive 2>non-primitive
+
+//primitive : call by value
+// string , number, boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail = undefined;// by default undefined value store under the userEmail
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const BigNumber = 47421545214124n
+
+
+
+//reference type or non primitive : call by reference //  type of nonprimitive datatype is object except function   
+//arrays , objects , Functions
+
+
+// javascript mastery elements -> (i)objects (ii)browser events 
+
+//javascript is a dynamically typed language,, because data type will automatically assigned at the time of execution
+
+
+const heros = ["shaktiman","nagraj","doga"];
+
+let myObj =
+{
+    name: "hitesh",
+    age: 22,
+
+}
+
+const myfunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof bigNumber);
+
+
+
