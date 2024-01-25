a86973107d0bedcf433ceeb86c70301337d61ed1
subhajit_dev <146475311+subhajit1311-dev@users.noreply.github.com>
2024-01-25 08:25:12 +0000

    comparison done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+/*console.log(2>1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+console.log(2>=1);
+*/
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0)
+console.log(null == 0)
+console.log(null>=0);
+//the reason behind the o/p is different is that an equality check and comparison work differently
+//comparison convert null to a number ,treating it as 0
+
+
+console.log(undefined == 0)
+console.log(undefined>0)
+console.log(undefined<0)
+
+//===
+
+console.log("2" === 2 );
