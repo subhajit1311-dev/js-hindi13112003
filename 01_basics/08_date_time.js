bd0086f692176bd4e382af5d4a907bb7d40a4794
subhajit_dev <146475311+subhajit1311-dev@users.noreply.github.com>
2024-01-26 15:42:45 +0000

    date  and time done

---
+++ 01_basics/08_date_time.js
@@ -0,0 +1,13 @@
+//date 
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
+
+let myCreateDate = new Date()
