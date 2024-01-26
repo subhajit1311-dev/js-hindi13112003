915420922b9ca2de11a84edbc92ca0b86f9d297a
subhajit_dev <146475311+subhajit1311-dev@users.noreply.github.com>
2024-01-26 11:52:48 +0000

    string done

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -5,7 +5,7 @@ const repoCount = 50
 console.log(`hello my name is ${name} and my repo count is ${repoCount} `); //string interpolation
 
 
-const gameName = new String('subhdsffgg') // string declaration: constructor type
+const gameName = new String('subha-jit-roy') // string declaration: constructor type
 console.log(gameName[0])//access key value pairs
 console.log(gameName.__proto__);//syntax of accesing prototype 
 
@@ -17,3 +17,19 @@ console.log(gameName.toUpperCase())//but original string  donot change in upperc
 console.log(gameName.charAt(3))// to find a character a given position
 
 console.log(gameName.indexOf('f'))
+
+const newstring = gameName.substring(0,4)
+console.log(newstring)
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString)//explore more in later
+
+const newstringOne = "    subhajit \n   "
+console.log(newstringOne);
+console.log(newstringOne.trim());
+
+const url = "https://subhajit.com/subhajit%20roy"
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('subhajit'));
+console.log(gameName.split('-'))
