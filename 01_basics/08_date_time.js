30c1f8d709670f5e75acf630f330497994e97091
subhajit_dev <146475311+subhajit1311-dev@users.noreply.github.com>
2024-01-27 15:36:30 +0000

    date and time done

--- 01_basics/08_date_time.js
+++ 01_basics/08_date_time.js
@@ -1,13 +1,47 @@
 //date 
 
 let myDate = new Date()
-console.log(myDate);
+/*console.log(myDate);
 console.log(myDate.toString());
 console.log(myDate.toDateString());
 console.log(myDate.toLocaleString());
 console.log(myDate.toISOString());
 console.log(myDate.toJSON());
 console.log(myDate.toLocaleDateString());
-console.log(typeof myDate);
+console.log(typeof myDate);*/
 
-let myCreateDate = new Date()
+//let myCreatedDate = new Date(2023,0,23) // month '0' die chalu hoy javascript e
+//console.log(myCreatedDate.toDateString());
+
+let myCreatedDate = new Date(2023,0,23,5,3)
+console.log(myCreatedDate.toLocaleString());
+
+let CreatedDate = new Date("2023-01-14") // yy-mm-dd format e thakle month 01 die chalu hoy
+console.log(CreatedDate.toLocaleString());
+
+let CreateDate = new Date("01-14-2023") // yy-mm-dd format e thakle month 01 die chalu hoy
+console.log(CreateDate.toLocaleString());
+
+let mytimestamp = Date.now()
+
+console.log(mytimestamp);
+console.log(CreateDate.getTime());// return value 1st jan 1970(standard) to till the declare date convert that time into miliseconds
+
+//important
+console.log(Math.floor(Date.now()/1000)) //latest date ke convert kore milisecond theke second e
+
+
+
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());//monday -> 1
+
+newDate.toLocaleString('default',{
+    weekday: "long",
+
+})
+
+
+
