7d3375b9668d07fcaed1578caf6207033ba07dc9
subhajit_dev <146475311+subhajit1311-dev@users.noreply.github.com>
2024-01-28 20:43:59 +0000

    function 1 done

---
+++ 03_basics/01_function.js
@@ -0,0 +1,25 @@
+console.log("S")
+console.log("U");
+console.log("B");
+console.log("H");
+console.log("A");
+console.log("J");
+console.log("I");
+console.log("T");
+
+function sayMyName()
+{
+    console.log("S");
+    console.log("U");
+    console.log("B");
+    console.log("H");
+    console.log("A");
+    console.log("J");
+    console.log("I");
+    console.log("T");
+
+}
+
+sayMyName();
+//function execution
+
