94e3db9e073d393261195567e7063c6d0def16a5
subhajit_dev <146475311+subhajit1311-dev@users.noreply.github.com>
2024-01-28 20:30:46 +0000

    obj3 done

---
+++ 02_basics/05_object3.js
@@ -0,0 +1,28 @@
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh",
+
+}
+
+const {courseInstructor: instructor} = course
+// console.log(courseInstructor)
+console.log(Instructor) // object destructuring
+
+//google login api er example
+
+// {
+//     "name": "subhajit",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+[
+    {},
+    {},
+    {}
+
+]
+
+//javascript object notation
+
+//json formatter ~ resources
