cf317765dee5100d7a3263c3f2d30e027458ccea
subhajit_dev <146475311+subhajit1311-dev@users.noreply.github.com>
2024-01-29 06:17:13 +0000

    function 1 done

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -26,8 +26,63 @@ console.log("Result: ",result);
 
 function loginUserMessage(username)
 {
-    re
+    if(username == undefined ) //!username
+    {
+        console.log("please enter a username");
+        return;
+    }
+    return `${username} just logged in`;
+    
 }
+
+
+//console.log(loginUserMessage("subhajit")) // if we cannot pass any thing then the o/p should be undefined
+// "" and undefined is false value
+
+function calculateCarPrice(val1,val2,...num1) // ... ke rest operator o bole and spread opeartor o bole but purpose different
+{
+    return num1; // function er under e rest operator pass hoy ebhabe
+}
+
+console.log(calculateCarPrice(200,400,500,355,980))
+
+
+const user = {
+    userName: "hitesh",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.userName} and price is ${anyobject.price}`)
+}
+
+//handleObject(user)
+handleObject({
+
+    username: "sam",
+    price: 399
+}
+)
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray)
+{
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+
+
+
+
+
+
+
+
+
+
+
 
 
 
