36bf51cc6b3aa21dbc1c8cb08af55c1d76ac9dfc
subhajit_dev <146475311+subhajit1311-dev@users.noreply.github.com>
2024-01-29 16:29:54 +0000

    callstack done

---
+++ 03_basics/05_callstack.js
@@ -0,0 +1,34 @@
+//execution context
+//javascript e kono code file dile seta duto phase e execute hoy
+
+//global ec made hoy -> {}
+//javascript is single threaded language
+
+//global execution context and function execution context
+//{} -> 1> memory creation phase 2>execution phase
+
+//step 1
+//global execution phase e memory ....this e allocate hoy
+
+//step 2
+//memory phase e val1 and val2 te undefined store hoy and addnum e function defination jai
+//reult1 and result2 undefined hoy
+
+//step 3
+//val1 e 10 and val2 e 5 assign hoy addnum e execution context toiri hoy jar moddhe thake new variable environment
+//ar execution thread... baaki copy te
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
