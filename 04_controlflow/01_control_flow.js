e0bd12c01722a6af67254a8d8543ae62a1df11a1
subhajit_dev <146475311+subhajit1311-dev@users.noreply.github.com>
2024-01-29 17:27:24 +0000

    if else done

---
+++ 04_controlflow/01_control_flow.js
@@ -0,0 +1,70 @@
+// if
+// <,>,<=,>=, == , !=   // === means number equal hobe tar sathe sathe datatype o equal hobe //!==
+// const isUserloggedIn = true;
+// if(2 === "2")
+// {
+//     console.log(executed);
+// }
+
+
+
+// const temperature = 41;
+// if(temperature === 41)
+// {
+//     console.log("less than 50")
+// }
+// else{
+//     console.log("temperature is greater than 50 ")
+// }
+
+// const score  = 200;
+// if(score>100)
+// {
+//     const power = "fly"
+//     console.log(`user power: ${power}`);
+// }
+
+
+// const balance =1000;
+// // if(balance > 500) console.log("test"); //implicit scope -> single line executable only
+// if(balance < 500)
+// {
+//     console.log("less than 500");
+// }
+// else if(balance < 750)
+// {
+//     console.log("less than 750");
+// }
+// else if(balance < 900)
+// {
+//     console.log("less than 900");
+// }
+// else 
+// {
+//     console.log("less than 1200");
+// }
+
+
+// const UserloggedIn = true;
+// const debitCard = true;
+// const loggedInFromGoogle = false;
+// const loggedInFromEmail = true;
+
+// if(UserloggedIn && debitCard)
+// {
+//     console.log("Allow to buy course");
+// }
+
+// if(loggedInFromEmail || loggedInFromEmail )
+// {
+//     console.log("user logged in");
+// }
+
+
+
+
+
+
+
+
+
