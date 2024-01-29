91d927973ab43f132759546d9a277554e2f05e5a
subhajit_dev <146475311+subhajit1311-dev@users.noreply.github.com>
2024-01-29 17:53:42 +0000

    switch done

---
+++ 04_controlflow/02_Switch.js
@@ -0,0 +1,40 @@
+//switch(key)
+// {
+//     case value: 
+//         break;
+//     default :
+//         break;
+
+// }
+
+const month = 3;
+
+// duplicate shortcut -> shift+alt+down arrow
+switch(month)
+{
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("February")
+        break;
+    case 3:
+        console.log("March") //code  kono option e execute korar por tar nicher baki statement o execute hobe except default case
+        break;
+    case 4:
+        console.log("april")
+        break;
+    default:
+        console.log("default case match")
+        break;
+    
+}
+
+
+
+
+
+
+
+
+
