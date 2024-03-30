0768d2be41d00cf75b2128e32f9745437315a1b7
subhajit_dev <146475311+subhajit1311-dev@users.noreply.github.com>
2024-03-30 07:31:42 +0000

    done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,7 @@
 const accountId = 144553
 let accountEmail = "subhajit@google.com"
-var accountPassword = "12345"
-accountcity = "jaipur"
+var accountPassword = "12345" //variable gulo meomory te assign kore value gulo
+//accountcity = "jaipur"
 let accountState;
 
 // accountId = 2   // not allowed
@@ -9,10 +9,10 @@ let accountState;
 
 accountEmail = "har@jjn.com"
 accountPassword = "544466654511"
-accountCity = "bengaluru"
+//accountCity = "bengaluru"
 
 /*
 prefer not to use var because the issue of in block scope and functional scope
 */
 console.log(accountId);
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState]) 
+console.table([accountId,accountEmail,accountPassword,accountState]) 
