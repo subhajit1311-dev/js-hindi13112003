4c2fe1a62d01d3263b11521b371960667d404a26
subhajit_dev <146475311+subhajit1311-dev@users.noreply.github.com>
2024-06-11 20:20:10 +0000

    done

--- 02_basics/01_array1.js
+++ 02_basics/01_array1.js
@@ -1,6 +1,6 @@
 //array
 
-const myArr = [0,1,2,3,4,5,6,7]
+const myArr = [0,1,2,3,4,5]
 //array is an object where we store multiple elements in a single variable
 
 //console.log(myArr[0]);
@@ -10,7 +10,7 @@ const myHeros = ["shaktiman","naagraj","hulk"]
 
 const myArr2 = new Array(1,2,3,4)
 //console.log(myArr2[0])
-console.log(myHeros[0]);
+//console.log(myHeros[0]);
 
 //ARRAY METHODS
 
@@ -19,12 +19,12 @@ myArr.push(7)
 myArr.pop()
 //console.log(myArr);
 
-//myArr.unshift(9) // element array er front side part die insert hoy but bad practice because er jonno 
+myArr.unshift(9) // element array er front side part die insert hoy but bad practice because er jonno 
 //sob element ke shift korte hoy next place e
-//console.log(myArr);
+console.log(myArr);
 
 myArr.shift()
-//console.log(myArr);
+console.log(myArr);
 
 //console.log(myArr.includes(9)) // datatype of this operation is boolean
 //console.log(myArr.indexOf(3))
@@ -32,10 +32,10 @@ myArr.shift()
 const newArr = myArr.join()
 // naturally join changed the array into a string and 
 
-//console.log(myArr)
+console.log(myArr)
 
-//console.log(newArr)
-//console.log(typeof newArr)
+console.log(newArr)
+console.log(typeof newArr)
 
 //slice,splice
 
@@ -47,8 +47,8 @@ console.log("B ",myArr);
 
 
 const myn2 = myArr.splice(1,3)
-console.log("c " , myArr);
-console.log(myn2);
+//console.log("c " , myArr);
+//console.log(myn2);
 
 //splice e main array manipulate hoye jai but slice e main array manipulate hoy na and the element remove o hoy na
 //till range is given in splice
