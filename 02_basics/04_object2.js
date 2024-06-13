e7a476647490baae6775d8e57867ebfbfbd394c8
subhajit_dev <146475311+subhajit1311-dev@users.noreply.github.com>
2024-06-13 15:15:09 +0000

    obj 2 done

--- 02_basics/04_object2.js
+++ 02_basics/04_object2.js
@@ -22,21 +22,21 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullname.userfullname.firstname)
+//console.log(regularUser.fullname.userfullname.firstname)
 
 const obj1 = {1: "a",2: "b"}
 const obj2 = {3: "a",4: "b"}
 
-//const obj3 = {obj1,obj2}
-//const obj3 = Object.assign({} , obj1, obj2)//{} -> target object .... and obj1 & obj2 is source object 
+//const obj3 = {obj1,obj2};
+//console.log(obj3)
+const obj3 = Object.assign({} , obj1 , obj2)//{} -> target object .... and obj1 & obj2 is source object 
 //we merge the source objects and  store it into the target object ... 
-
-//console.log(obj3);
+console.log(obj3);
 
 
 //spread
-const obj3 = {...obj1,...obj2}
-console.log(obj3);
+//const obj3 = {...obj1,...obj2}
+//console.log(obj3);
 
 
 //database 
@@ -52,16 +52,16 @@ const users = [
     }
 ]
 users[1].email
-console.log(tinderUser);
+//console.log(tinderUser);
 
 
 // database usecases
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser))
-console.log(Object.entries(tinderUser))
+//console.log(Object.keys(tinderUser));
+//console.log(Object.values(tinderUser))
+//console.log(Object.entries(tinderUser))
 
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+//console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
 
 
