33db819994936aca652202cfb2e553a2aa4dfc70
subhajit_dev <146475311+subhajit1311-dev@users.noreply.github.com>
2024-06-18 16:09:00 +0000

    basic async done

---
+++ 09_async/basic.js
@@ -0,0 +1,16 @@
+//javascript -> synchronous singleThreaded
+//cannot run without support or runtime environment
+//(by default)
+
+//execution context
+//execute one line at a time
+//each operation waits for the last one to complete before executing
+
+//Blocking code vs non blocking code
+//blocking code->block the flow of program eg. readfile sync
+//non blocking code -> does not block execution eg. readfile async
+
+//blocking code ar nonblocking code er moddhe ke bhalo seta dekte gale differentg usecases ache
+
+
+
