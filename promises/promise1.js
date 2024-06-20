2cbbb80d6d188ef9661d18e31642cbb76723108d
subhajit_dev <146475311+subhajit1311-dev@users.noreply.github.com>
2024-06-20 14:09:58 +0000

    promise done

--- promises/promise1.js
+++ promises/promise1.js
@@ -98,3 +98,14 @@ async function getAllUsers()
 }
 
 getAllUsers();
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json();
+})
+.then((data)=> console.log(data))
+.catch((error)=>
+{
+    console.log(error);
+});
+
