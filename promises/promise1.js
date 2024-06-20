412b3e2099ee9ac643d95875122e888c239a3636
subhajit_dev <146475311+subhajit1311-dev@users.noreply.github.com>
2024-06-20 12:59:16 +0000

    ok

--- promises/promise1.js
+++ promises/promise1.js
@@ -1,6 +1,8 @@
 // pending: initial state, neither fulfilled nor rejected.
 // fulfilled: meaning that the operation was completed successfully.
 // rejected: meaning that the operation failed.
+
+
 // const promiseOne = new Promise(function(resolve,reject){
 //     //do an async task
 //     //db calls,crtyptography,file system
@@ -34,14 +36,14 @@
 //     console.log(user);
 // })
 
-const promiseFour= new Promise(function(reject,resolve){
+const promiseFour= new Promise(function(resolve,reject){
     setTimeout(function(){
         let error = true;
         if(!error){
             resolve({username:"hitesh",password:"123"});
         }
         else{
-            reject('something went wrong');
+            reject('ERROR: something went wrong');
         }
     },1000)
 })
@@ -51,6 +53,48 @@ promiseFour.then((user)=>{
 }).then((username)=>{
     console.log(username);
 }).catch(function(error){
-    console.log(error)
-})
+     console.log(error)
+}).finally(()=>{console.log("the promise is either resolve or rejected")});
 
+
+const promiseFive= new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = false;
+        if(!error){
+            resolve({username:"javascript",password:"123"});
+        }
+        else{
+            reject('ERROR: js went wrong');
+        }
+    },1000)
+});
+
+
+async function consumePromiseFive()
+{
+    try
+    {
+        const response = await promiseFive; 
+        console.log(response);
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
+}
+consumePromiseFive()
+
+async function getAllUsers()
+{
+    try{
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json();
+    console.log(data);
+    }
+    catch(error)
+    {
+        console.log("E: ",error);
+    }
+}
+
+getAllUsers();
